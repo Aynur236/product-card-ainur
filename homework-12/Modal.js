2994da1795979fd147807b56e5eb12adfb226eec
Ainur <yt.frost@mail.ru>
2026-03-23 17:24:50 +0300

    properly remove event listeners on modal close

--- homework-12/Modal.js
+++ homework-12/Modal.js
@@ -2,44 +2,57 @@ export class Modal {
   constructor(modalId, buttonId, shouldCloseOnOverlay) {
     this.modal = document.getElementById(modalId);
     this.overlay = document.getElementById('overlay');
+    this.closeButton = this.modal.querySelector('#modal-close-button');
+    this.shouldCloseOnOverlay = shouldCloseOnOverlay;
     this.#initOpen(buttonId);
-    this.#initClose(shouldCloseOnOverlay);
   }
 
+  #handleCloseClick = () => {
+    this.close();
+  };
+
+  #handleOverlayClick = () => {
+    if (this.isOpen()) {
+      this.close();
+    }
+  };
+  
   open() {
     this.modal.classList.add('modal-showed');
     this.overlay.classList.add('overlay-showed');
-  }
+    this.#initClose();
+  };
 
   close() {
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay-showed');
-  }
+    this.#removeCloseListeners();
+  };
 
   isOpen() {
     return this.modal.classList.contains('modal-showed');
-  }
+  };
 
   #initOpen(buttonId) {
     const button = document.getElementById(buttonId);
     button.addEventListener('click', () => {
       this.open();
-    })
+    });
+  };
+
+  #initClose() {
+    this.closeButton.addEventListener('click', this.#handleCloseClick);
+
+    if (this.shouldCloseOnOverlay) {
+      this.overlay.addEventListener('click', this.#handleOverlayClick);
+    }
   }
 
-  #initClose(shouldCloseOnOverlay) {
-    const closeButton = this.modal.querySelector('#modal-close-button')
+  #removeCloseListeners() {
+    this.closeButton.removeEventListener('click', this.#handleCloseClick);
 
-    closeButton.addEventListener('click', () => {
-      this.close();
-    })
-
-    if (shouldCloseOnOverlay) {
-      this.overlay.addEventListener('click', () => {
-        if (this.isOpen()) {
-          this.close();
-        }
-      })
+    if (this.shouldCloseOnOverlay) {
+      this.overlay.removeEventListener('click', this.#handleOverlayClick);
     }
   }
 }
